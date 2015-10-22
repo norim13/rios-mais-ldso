01644e9ad304efe3929c2c50ace3f3b3831acb7e
Filipe Miranda <fil.fmiranda@gmail.com>
2015-10-22 18:02:40 +0100

    navbar com a cor ja da em andorid 4.4 e inferior

--- app/app/src/main/java/engenheiro/rios/SubActivity.java
+++ app/app/src/main/java/engenheiro/rios/SubActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class SubActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class SubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Guarda Rios");
         setSupportActionBar(toolbar);
 
 
@@ -26,5 +28,14 @@ public class SubActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_homepage, menu);
+
+        return true;
+    }
+
 
 }
