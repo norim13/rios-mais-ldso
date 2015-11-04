0dd8bca61ea2cbdc643c595d2892ddd7215a43d5
norim13 <norim_13@hotmail.com>
2015-11-04 04:49:53 +0000

    cenas mail. ainda n dá.

--- website/config/environments/production.rb
+++ website/config/environments/production.rb
@@ -77,9 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'rios.pt' }
+  config.action_mailer.default_url_options = { :host => 'http://riosmais.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.raise_delivery_errors = true
 
 end
