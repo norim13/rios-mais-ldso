3190616134e4fd65be58295d67db18ebf24afb6c
Filipe Miranda <fil.fmiranda@gmail.com>
2015-11-14 19:42:03 +0000

    Reads Location GPS (app

--- app/app/src/main/java/engenheiro/rios/Homepage.java
+++ app/app/src/main/java/engenheiro/rios/Homepage.java
@@ -2,6 +2,7 @@ package engenheiro.rios;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,38 +10,71 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
 
 import engenheiro.rios.DataBases.DB_functions;
 import engenheiro.rios.DataBases.User;
 import engenheiro.rios.IRR.IRR_question;
+public class Homepage extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-public class Homepage extends AppCompatActivity {
+    private Bundle savedInstanceState;
 
-    User current_user;
-    public static final String PREFS_NAME = "UserInfo";
-    String token;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView tvLatlong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        tvLatlong = (TextView) findViewById(R.id.gps);
+
+        buildGoogleApiClient();
+
+        if(mGoogleApiClient!= null){
+            mGoogleApiClient.connect();
+        }
+        else
+            Toast.makeText(this, "Not connected...", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    User current_user;
+    public static final String PREFS_NAME = "UserInfo";
+    String token;
+
+    @Override
+    public void onConnectionFailed(ConnectionResult arg0) {
+        Toast.makeText(this, "Failed to connect...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnected(Bundle arg0)  {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.homepage_title);
         setSupportActionBar(toolbar);
 
         current_user = new User();
-
+        tvLatlong = (TextView) this.findViewById(R.id.gps);
 
         // Restore preferences
-        token="";
+        token = "";
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        token=settings.getString("token","-1");
+        token = settings.getString("token", "-1");
         Log.e("token", ": " + token);
         try {
             DB_functions.saveForm();
@@ -49,6 +83,32 @@ public class Homepage extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            tvLatlong.setText("Latitude: "+ String.valueOf(mLastLocation.getLatitude())+"Longitude: "+
+                    String.valueOf(mLastLocation.getLongitude()));
+        }
+
+
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Toast.makeText(this, "Connection suspended...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
@@ -87,8 +147,6 @@ public class Homepage extends AppCompatActivity {
     }
 
 
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -116,4 +174,5 @@ public class Homepage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
