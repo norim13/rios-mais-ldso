4d6f7bbe89a0ab2b665d99d5bc87d4400834fbde
André Pires <andre.r.o.pires@gmail.com>
2015-11-24 20:02:15 +0000

    fixed calculate IRR for selects
    fixed show IRR in rio profile

--- website/app/controllers/rio_controller.rb
+++ website/app/controllers/rio_controller.rb
@@ -8,15 +8,14 @@ class RioController < ApplicationController
       @has_irr = 'no'
     else
       @has_irr = 'yes'
-      @avg_hidrogeomorfologia = @form_irrs.average(:irr_hidrogeomorfologia).round(2)
-      @avg_qualidadedaagua = @form_irrs.average(:irr_qualidadedaagua).round(2)
-      @avg_alteracoesantropicas = @form_irrs.average(:irr_alteracoesantropicas).round(2)
-      @avg_corredorecologico = @form_irrs.average(:irr_corredorecologico).round(2)
-      @avg_participacaopublica = @form_irrs.average(:irr_participacaopublica).round(2)
-      @avg_organizacaoeplaneamento = @form_irrs.average(:irr_organizacaoeplaneamento).round(2)
-      @avg_irr = @form_irrs.average(:irr).round(2)
+      @avg_hidrogeomorfologia =@form_irrs.max_by(&:irr_hidrogeomorfologia).irr_hidrogeomorfologia
+      @avg_qualidadedaagua = @form_irrs.max_by(&:irr_qualidadedaagua).irr_qualidadedaagua
+      @avg_alteracoesantropicas = @form_irrs.max_by(&:irr_alteracoesantropicas).irr_alteracoesantropicas
+      @avg_corredorecologico = @form_irrs.max_by(&:irr_corredorecologico).irr_corredorecologico
+      @avg_participacaopublica = @form_irrs.max_by(&:irr_participacaopublica).irr_participacaopublica
+      @avg_organizacaoeplaneamento = @form_irrs.max_by(&:irr_organizacaoeplaneamento).irr_organizacaoeplaneamento
       @irr_total = [@avg_hidrogeomorfologia,@avg_qualidadedaagua,@avg_alteracoesantropicas,
-          @avg_corredorecologico, @avg_participacaopublica, @avg_organizacaoeplaneamento].max().round(2)
+          @avg_corredorecologico, @avg_participacaopublica, @avg_organizacaoeplaneamento].max()
 
       @imgs = []
       @form_irrs.each do |f|
