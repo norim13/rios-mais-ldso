709f9ef5ead3bd7fa8ae3b58fda9389ae2111263
Filipe Batista <filipedgb@gmail.com>
2015-12-02 19:44:57 +0000

    Limpezas solucoes v1

---
+++ app/app/src/main/java/ldso/rios/Form/LimpezaSolucoes.java
@@ -0,0 +1,48 @@
+package ldso.rios.Form;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ldso.rios.R;
+
+public class LimpezaSolucoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_limpeza_solucoes);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject((String) this.getIntent().getSerializableExtra("solucoes"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+        Log.e("objecto: ", jsonObject.toString());
+
+    }
+
+}
