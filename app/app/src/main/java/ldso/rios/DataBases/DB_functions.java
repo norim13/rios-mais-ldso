df7b561c909b717849a0e7cd78e15390d9525ef2
Filipe Miranda <fil.fmiranda@gmail.com>
2015-12-16 21:33:36 +0000

    ja aparecem imagens nos guardarios

--- app/app/src/main/java/ldso/rios/DataBases/DB_functions.java
+++ app/app/src/main/java/ldso/rios/DataBases/DB_functions.java
@@ -36,6 +36,7 @@ import ldso.rios.Form.IRR.ViewFormIRR;
 import ldso.rios.Form.LimpezaSolucoes;
 import ldso.rios.Form.Sos_rios;
 import ldso.rios.MainActivities.Form_IRR_mainActivity;
+import ldso.rios.MainActivities.GuardaRios;
 import ldso.rios.MainActivities.Limpeza;
 import ldso.rios.MainActivities.Profile;
 import ldso.rios.MainActivities.ProfileEditActivity;
@@ -49,7 +50,7 @@ import ldso.rios.Mapa_Rotas;
 
 public class DB_functions {
 
-    static String base_url="http://riosmais.herokuapp.com";
+    public static String base_url="http://riosmais.herokuapp.com";
 
     public static void saveUser(final String nome, final String email, final String password, final String password_confirmation, final String telef, final String profissao, final String habilitacoes, final Boolean formacao, final Register register_class) throws IOException, JSONException {
 
@@ -1983,6 +1984,85 @@ public class DB_functions {
                         System.out.println(name);
                         System.out.println(url);
                     }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+
+
+    public static void getGuardaRios(final GuardaRios guardaRios) throws IOException, JSONException {
+
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                String url = base_url+"/api/v2/guardarios";
+
+                URL obj = null;
+                try {
+                    obj = new URL(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpURLConnection con = null;
+                try {
+                    con = (HttpURLConnection) obj.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // optional default is GET
+                try {
+                    con.setRequestMethod("GET");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+
+                //add request header
+                con.setRequestProperty("Content-Type", "application/json");
+
+                int responseCode = 0;
+                try {
+                    responseCode = con.getResponseCode();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("\nSending 'GET' request to URL : " + url);
+                System.out.println("Response Code : " + responseCode);
+
+                BufferedReader in = null;
+                try {
+                    in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                try {
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //print result
+                System.out.println(response.toString());
+                Log.e("teste", response.toString());
+
+                try {
+                    guardaRios.images(response.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
--- app/app/src/main/java/ldso/rios/MainActivities/GuardaRios.java
+++ app/app/src/main/java/ldso/rios/MainActivities/GuardaRios.java
@@ -1,17 +1,38 @@
 package ldso.rios.MainActivities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import ldso.rios.DataBases.DB_functions;
 import ldso.rios.Form.GuardaRios_form;
 import ldso.rios.R;
 
 public class GuardaRios extends AppCompatActivity {
+
+
+    GridLayout gridLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,15 @@ public class GuardaRios extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        try {
+            DB_functions.getGuardaRios(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        gridLayout = (GridLayout) findViewById(R.id.girdLayout);
     }
 
 
@@ -50,4 +80,134 @@ public class GuardaRios extends AppCompatActivity {
     }
 
 
+    public void images(final String s) throws JSONException {
+        new Thread()
+        {
+            public void run()
+            {
+                GuardaRios.this.runOnUiThread(new Runnable()
+                {
+                    public void run() {
+                        try {
+                            JSONObject g= new JSONObject(s);
+                            Log.e("entrou aqui","");
+                            final JSONArray array= g.getJSONArray("guardarios");
+                            Log.e("entrou aqui","");
+                            for(int i =0;i<array.length();i++)
+                            {
+                                if (i==0)
+                                {
+                                    final int finalI = i;
+                                    Thread thread = new Thread(new Runnable(){
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                JSONObject guardario= array.getJSONObject(finalI);
+
+                                                JSONObject array_imagens= (JSONObject) guardario.get("image");
+
+                                                final String url=DB_functions.base_url+array_imagens.get("url");
+                                                Log.e("url",url);
+
+                                                final ImageView imageView= (ImageView) findViewById(R.id.imageView);
+                                                final Drawable d=LoadImageFromWebOperations(url);
+                                                final Bitmap b= LoafImageURL(url);
+
+                                                new Thread()
+                                                {
+                                                    public void run()
+                                                    {
+                                                        GuardaRios.this.runOnUiThread(new Runnable()
+                                                        {
+                                                            public void run() {
+                                                                imageView.setImageBitmap(b);
+
+
+
+                                                            }
+                                                        });
+                                                    }
+                                                }.start();
+
+
+
+
+
+
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    });
+
+                                    thread.start();
+                                }
+
+
+
+
+
+
+
+                            }
+
+
+
+                        } catch (Exception e){
+
+                        }
+
+
+
+                    }
+                });
+            }
+        }.start();
+
+
+    }
+
+    public Bitmap LoafImageURL(String url) throws IOException {
+        URL url_value = new URL(url);
+        Bitmap srcBmp =BitmapFactory.decodeStream(url_value.openConnection().getInputStream());
+        Bitmap dstBmp;
+
+        if (srcBmp.getWidth() >= srcBmp.getHeight()){
+
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    srcBmp.getWidth()/2 - srcBmp.getHeight()/2,
+                    0,
+                    srcBmp.getHeight(),
+                    srcBmp.getHeight()
+            );
+
+        }else{
+
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    0,
+                    srcBmp.getHeight()/2 - srcBmp.getWidth()/2,
+                    srcBmp.getWidth(),
+                    srcBmp.getWidth()
+            );
+        }
+        return dstBmp;
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+
+
+
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            Log.e("nao nulo","img nao nula");
+            return d;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("nulo","img nula");
+            return null;
+        }
+    }
 }
